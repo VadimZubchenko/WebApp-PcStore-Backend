420b7ed93dcafdb1c76c411547a67f6575fd4c33
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2024-03-12 15:47:46 +0200

    the adding parts option has been added

--- src/main/java/com/vadimzu/webpcstore/controller/PartController.java
+++ src/main/java/com/vadimzu/webpcstore/controller/PartController.java
@@ -1,12 +1,14 @@
 package com.vadimzu.webpcstore.controller;
 
-
+import com.vadimzu.webpcstore.entity.PartEntity;
 import com.vadimzu.webpcstore.exception.ResourceNotFoundException;
 import com.vadimzu.webpcstore.service.PartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -20,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class PartController {
-    
+
     @Autowired
     private PartService partService;
 
@@ -37,4 +39,17 @@ public class PartController {
         }
     }
 
+    //add new part
+    @PostMapping("/parts")
+
+    public ResponseEntity<?> addParts(@RequestBody PartEntity part) {
+        try {
+            partService.addPart(part);
+            return ResponseEntity.status(201).body("Part has been added.");
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Request didn't pass throw");
+        }
+
+    }
 }
