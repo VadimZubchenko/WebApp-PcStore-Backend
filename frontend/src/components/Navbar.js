85722c8c037de8f684f8acfcf439ae9497aa90c6
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2026-09-15 20:54:51 +0300

    Test new nginx configuration

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -70,7 +70,7 @@ const Navbar = (props) => {
       <div className="container mx-auto p-0">
         <div className="row">
           <div className="col">
-            <p className="navbar-brand mt-3">PCStore Manager – DEV</p>
+            <p className="navbar-brand mt-3">PCStore Manager /dev</p>
 
             <button
               className="navbar-toggler"
